7a8382c1480ed4d53fa2178396b830ebd8f3cf4b
Federico <fyde90@gmail.com>
2025-04-14 12:59:59 +0200

    better variable names

--- main.cpp
+++ main.cpp
@@ -77,7 +77,7 @@ int main(int argc, char *argv[]) {
   //
   const float minBarWidth = 2.0;
   // needs to be outside the loop because it will average out for smoothing
-  std::vector<double> barHeights(SAMPLE_SIZE, 0.0);
+  std::vector<double> yPositions(SAMPLE_SIZE, 0.0);
 
   while (window.isOpen()) {
     while (const std::optional event = window.pollEvent()) {
@@ -123,13 +123,13 @@ int main(int argc, char *argv[]) {
       // clear the window
       window.clear(sf::Color::Black);
 
-      // calculate bars height, width and position based on magnitudes
-      // each iteration draws the previous bar
-      // so we can calculate the width after knowing where the next bar will be
-      // variables to save info about previous bar
+      // calculate position and width based on magnitudes
+      // each iteration draws the previous item (bar or vertex)
+      // so we can calculate the width after knowing where the next item will be
+      // variables to save info about previous item
       float prevX = 0.0f;
-      float prevHeight = 0.0f;
-      // adding dummy value at the end for extra iteration to draw the last bar
+      float prevY = 0.0f;
+      // adding dummy value at the end for extra iteration to draw the last item
       magnitudes.push_back(0.0);
       //
       std::vector<double> xPositions(magnitudes.size(), 0.0);
@@ -139,7 +139,7 @@ int main(int argc, char *argv[]) {
       for (size_t i = 0; i < magnitudes.size(); ++i) {
         // convert to log scale
         double db = 20.0 * std::log10(magnitudes[i] + 1e-12);
-        // calculate frequency of bar
+        // calculate frequency of item
         double frequency = static_cast<double>(i * SAMPLE_RATE) / SAMPLE_SIZE;
         // calculate tilt for this frequency
         double tiltDb = TILT * std::log2(frequency / TILT_REF_FREQ);
@@ -148,10 +148,10 @@ int main(int argc, char *argv[]) {
         // remove everything below minDb
         db = std::max(minDb, db);
         // scale to window size
-        double height = ((db - minDb) / (maxDb - minDb)) * window.getSize().y;
+        double y = ((db - minDb) / (maxDb - minDb)) * window.getSize().y;
         // smooth over time (EMA)
-        barHeights[i] =
-            SMOOTHING_FACTOR * height + (1 - SMOOTHING_FACTOR) * barHeights[i];
+        yPositions[i] =
+            SMOOTHING_FACTOR * y + (1 - SMOOTHING_FACTOR) * yPositions[i];
 
         // frequency to log scale, for positioning and width
         double freqLog = (std::log2(frequency) - std::log2(minFrequency)) /
@@ -160,7 +160,7 @@ int main(int argc, char *argv[]) {
         float xPosition = window.getSize().x * freqLog;
 
         // don't draw the first iteration
-        // the second iteration draws the first bar, etc.
+        // the second iteration draws the first item, etc.
         if (i > 0) {
 
           if (mode == bars) {
@@ -170,10 +170,9 @@ int main(int argc, char *argv[]) {
             float barX = ((xPosition + prevX) / 2.0f) - width / 2.0f;
             // create bar and set properties
             sf::RectangleShape bar;
-            bar.setSize(sf::Vector2f(width, prevHeight));
+            bar.setSize(sf::Vector2f(width, prevY));
             bar.setOrigin(sf::Vector2f(width / 2.0f, 0));
-            bar.setPosition(
-                sf::Vector2f(barX, window.getSize().y - prevHeight));
+            bar.setPosition(sf::Vector2f(barX, window.getSize().y - prevY));
 
             window.draw(bar);
           }
@@ -181,14 +180,14 @@ int main(int argc, char *argv[]) {
           if (mode == line) {
             // line vertex
             lineVert[i].position =
-                sf::Vector2f(prevX, window.getSize().y - prevHeight);
+                sf::Vector2f(prevX, window.getSize().y - prevY);
             lineVert[i].color = sf::Color::White;
           }
         }
 
         //
         prevX = xPosition;
-        prevHeight = barHeights[i];
+        prevY = yPositions[i];
       }
 
       if (mode == line) {
